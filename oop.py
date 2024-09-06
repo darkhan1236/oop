d7cc971437eae2423a7554d96d42a04d974b8c30
darkhan1236 <dzejnulla194@gmail.com>
2024-09-06 22:47:09 +0500

    first commit

---
+++ oop.py
@@ -0,0 +1,18 @@
+# Пример создания списка
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+
+# Добавление элемента в список
+my_list.append(6)
+print(my_list)
+
+# Удаление элемента из списка
+my_list.remove(3)
+print(my_list)
+
+# Доступ к элементу по индексу
+print(my_list[2])
+
+# Пример создания словаря
+my_dict = {'name': 'Alice', 'age': 25, 'city': 'New York'}
+print(my_dict) 
